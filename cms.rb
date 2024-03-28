12e322c72714e71f143221f84606207b6b69b7ff
William <wguiraud@protonmail.com>
2024-03-28 10:06:00 -0700

    adding content to the txt files and cleaning up the cms.rb file.

--- cms.rb
+++ cms.rb
@@ -6,7 +6,6 @@ require 'tilt/erubis'
 root = File.expand_path("..", __FILE__)
 
 get "/" do 
-  #@files = Dir.children('data')
   @files = Dir.glob(root + "/data/*").map do |path|
     File.basename(path)
   end
