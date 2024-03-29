072ea612e377ee6f20a361c4370facb7022dc532
William <wguiraud@protonmail.com>
2024-03-29 16:39:46 -0700

    removing the comment related to the setting of the RACK_ENV key set to test saying that by setting the key to reference test the webserver is prevented from running.

--- test/cms_test.rb
+++ test/cms_test.rb
@@ -1,4 +1,4 @@
-ENV["RACK_ENV"]="test" # to let the sinatra object know that it doesn't have to run the web server when the tests are running!
+ENV["RACK_ENV"]="test" 
 
 require 'minitest/autorun'
 require 'rack/test'
