1f550988786ebfa8bb0a38963e3657f868bbd6ea
William <wguiraud@protonmail.com>
2024-04-02 11:33:55 -0700

    duplicating the about.md file and converting into a .txt file. implementing two Sinatra public instance methods one to render a markdown file to HTML and another that loads the file content according to its extention.

--- cms.rb
+++ cms.rb
@@ -11,6 +11,22 @@ end
 
 root = File.expand_path("..", __FILE__)
 
+def render_markdown(text)
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+  markdown.render(text)
+end
+
+def load_file_content(path)
+  content = File.read(path)
+  case File.extname(path)
+  when ".txt"
+    headers["Content-Type"] = "text/plain"
+    content
+  when ".md"
+    render_markdown(content)
+  end
+end
+
 get "/" do 
   @files = Dir.glob(root + "/data/*").map do |path|
     File.basename(path)
@@ -22,9 +38,8 @@ end
 get "/:file_name" do 
   file_path = root + "/data/" + params[:file_name]
 
-  if File.file?(file_path) 
-    headers["Content-Type"] = "text/plain"
-    File.read(file_path)
+  if File.exist?(file_path) 
+    load_file_content(file_path)
   else 
     session[:error] = "#{params[:file_name]} doesn't exist!" 
     redirect "/" 
